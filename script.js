99cfbd681053580627ce3acf2f9dc81da7626d58
belmontecarbonari-commits <belmontecarbonari@gmail.com>
2025-08-31 21:18:31 -0300

    Summary

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// Mostrar contacto al hacer click en el menú
+// Mostrar contacto al hacer clic en el menú
 document.getElementById("mostrar-contacto").addEventListener("click", function(e) {
     e.preventDefault();
     document.getElementById("contacto").style.display = "block";
+});
+
+// Evitar refresh en el formulario
+document.getElementById("form-contacto").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Mensaje enviado ✅");
+    this.reset();
 });
