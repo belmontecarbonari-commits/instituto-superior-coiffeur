7b22d5ad62c6f2d1d51c934eac974185a4a2c159
belmontecarbonari-commits <belmontecarbonari@gmail.com>
2025-08-31 21:15:34 -0300

    Summary

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Mostrar la sección contacto al hacer click en el menú
+// Mostrar contacto al hacer click en el menú
 document.getElementById("mostrar-contacto").addEventListener("click", function(e) {
     e.preventDefault();
     document.getElementById("contacto").style.display = "block";
